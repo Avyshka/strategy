de21b929e49770cde845984c86dc309e07a607ba
Aivar Sergeev <avyshka@yandex.ru>
2022-02-23 01:19:20 +0300

    Added uniRx, game menu and timer

---
+++ Assets/Code/UserControlSystem/UI/Presenter/TopPanelPresenter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Aivagames.Strategy.Abstractions;
+using TMPro;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace Aivagames.Strategy.UserControlSystem.UI.Presenter
+{
+    public class TopPanelPresenter : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _inputField;
+        [SerializeField] private Button _menuButton;
+        [SerializeField] private GameObject _menuGameObject;
+
+        [Inject]
+        private void Init(ITimeModel timeModel)
+        {
+            timeModel.GameTime.Subscribe(seconds =>
+            {
+                var t = TimeSpan.FromSeconds(seconds);
+                _inputField.text = $"{t.Minutes:D2}:{t.Seconds:D2}";
+            });
+
+            _menuButton
+                .OnClickAsObservable()
+                .Subscribe(_ => _menuGameObject.SetActive(true));
+        }
+    }
+}
