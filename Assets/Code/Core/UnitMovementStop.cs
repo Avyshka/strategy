c22a7b4d4c1bf76053af6ed4ec6b7eefa52797e0
Aivar Sergeev <avyshka@yandex.ru>
2022-03-04 01:44:40 +0300

    Added movement stop

--- Assets/Code/Core/UnitMovementStop.cs
+++ Assets/Code/Core/UnitMovementStop.cs
@@ -1,15 +1,49 @@
 ﻿using System;
+using Aivagames.Strategy.Abstractions;
 using Code.Utils;
 using UnityEngine;
 using UnityEngine.AI;
+using UniRx;
 
 namespace Aivagames.Strategy.Core
 {
     public class UnitMovementStop : MonoBehaviour, IAwaitable<AsyncExtensions.Void>
-    {
+    { 
         public event Action OnStop;
 
         [SerializeField] private NavMeshAgent _agent;
+        [SerializeField] private CollisionDetector _collisionDetector;
+        [SerializeField] private int _throttleFrames = 60;
+        [SerializeField] private int _continuityThreshold = 10;
+
+        private void Awake()
+        {
+            _collisionDetector.Collisions
+                .Where(_ => _agent.hasPath)
+                .Where(collision => collision.collider.GetComponentInParent<IUnit>() != null)
+                .Select(_ => Time.frameCount)
+                .Distinct()
+                .Buffer(_throttleFrames)
+                .Where(buffer =>
+                {
+                    for (var i = 1; i < buffer.Count; i++)
+                    {
+                        if (buffer[i] - buffer[i - 1] > _continuityThreshold)
+                        {
+                            return false;
+                        }
+                    }
+
+                    return true;
+                })
+                .Subscribe(_ =>
+                {
+                    _agent.isStopped = true;
+                    _agent.ResetPath();
+                    OnStop?.Invoke();
+                })
+                .AddTo(this);
+        }
 
         private void Update()
         {
